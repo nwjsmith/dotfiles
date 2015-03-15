c70251058c0bad3b1522159a7f3a34784aead531
Nate Smith <nate.smith@thescore.com>
2015-03-15 10:20:49 -0400

    Use fish shell for a bit

---
+++ config/fish/config.fish
@@ -0,0 +1,5 @@
+set -x FISH_HOME "$HOME/.config/fish"
+
+source "$FISH_HOME/exports.fish"
+source "$FISH_HOME/aliases.fish"
+source "$FISH_HOME/solarized.fish"
---
+++ config/fish/exports.fish
@@ -0,0 +1,3 @@
+set -x PATH /usr/local/sbin /usr/local/bin $PATH
+set -x EDITOR vim
+set -g fish_greeting ""

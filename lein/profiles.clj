f999a461815298dd549047898f477b3576eac346
Nate Smith <nwjsmith@gmail.com>
2016-07-31 20:54:40 -0400

    Add Leininingen configuration

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+{:repl {:plugins [[cider/cider-nrepl "0.13.0"]
+                  [refactor-nrepl "2.2.0"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}

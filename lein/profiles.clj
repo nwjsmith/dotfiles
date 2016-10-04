b18d2b44aa8f4029dbaceb231c5800c2e00ce07e
Nate Smith <nwjsmith@gmail.com>
2016-10-04 17:36:03 -0400

    Add lein-ancient
    Because I need the new hotness

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,5 +4,6 @@
                        [org.clojure/tools.nrepl "0.2.12"]]}
  :user {:dependencies [[slamhound "1.5.5"]]
         :plugins [[jonase/eastwood "0.2.3"]
+                  [lein-ancient "0.6.10"]
                   [lein-kibit "0.1.2"]
                   [slamhound "1.5.5"]]}}

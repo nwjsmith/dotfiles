61fdd9a1ff9745982b7c000178de0aeba7acaa9f
Nate Smith <nwjsmith@gmail.com>
2017-02-10 10:06:14 -0500

    Update Clojure support

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,8 @@
 {:repl {:plugins [[cider/cider-nrepl "0.14.0"]
                   [refactor-nrepl "2.2.0"]]
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}
+                       [org.clojure/tools.nrepl "0.2.12"]
+                       [org.clojure/tools.trace "0.7.9"]]}
  :user {:dependencies [[slamhound "1.5.5"]]
         :plugins [[jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.10"]

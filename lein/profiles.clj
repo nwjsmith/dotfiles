fe67e36123d4928899bd40f5542e2867b4b35824
Nate Smith <nwjsmith@gmail.com>
2017-04-29 13:03:02 -0400

    Don't BYOC

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,3 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
-                  [cider/cider-nrepl "0.14.0"]
                   [lein-cloverage "1.0.9"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

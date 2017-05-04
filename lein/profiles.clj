2719e22bdb45501da356c9c6e53ce666cb509ec2
Nate Smith <nwjsmith@gmail.com>
2017-05-04 09:32:43 -0700

    Linting for Clojure

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
-                  [lein-cloverage "1.0.9"]]
+                  [lein-cloverage "1.0.9"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

0183c320689c4077f0024a63f38ec1559556e831
Nate Smith <nwjsmith@gmail.com>
2017-05-10 21:10:45 -0400

    I'll bring my own Clojure, thank you very much

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
-                  [lein-cloverage "1.0.9"]
-                  [jonase/eastwood "0.2.3"]
+{:user {:plugins [[jonase/eastwood "0.2.3"]
+                  [lein-ancient "0.6.10"]
+                  [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
+                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
                   [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

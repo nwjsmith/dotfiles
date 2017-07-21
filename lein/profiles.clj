d2f089de126ac4666f5989156e68ffa0c8a14438
Nate Smith <nwjsmith@gmail.com>
2017-07-21 10:23:49 -0400

    Cider 0.15.0

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,6 @@
                   [lein-ancient "0.6.10"]
                   [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-                  [lein-pprint "1.1.2"]]
-        :dependencies [[org.clojure/tools.trace "0.7.9"]]}}
+                  [lein-pprint "1.1.2"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.15.0"]
+                  [org.clojure/tools.trace "0.7.9"]]}}

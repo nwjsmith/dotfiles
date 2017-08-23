1738b517617cbefd9a6902928e7ae26c4da96f11
Nate Smith <nwjsmith@gmail.com>
2017-08-23 08:01:11 -0400

    Try continuous testing

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,6 +2,7 @@
                   [lein-ancient "0.6.10"]
                   [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-                  [lein-pprint "1.1.2"]]}
+                  [lein-pprint "1.1.2"]
+                  [com.jakemccrary/lein-test-refresh "0.20.0"]]}
  :repl {:plugins [[cider/cider-nrepl "0.15.0"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

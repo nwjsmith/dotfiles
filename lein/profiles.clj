1ea4010849d209f371dc4f2a846a69588028262d
Nate Smith <nwjsmith@gmail.com>
2017-08-25 15:12:23 -0400

    Add slamhound

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,10 @@
-{:user {:plugins [[jonase/eastwood "0.2.3"]
+{:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :dependencies [[slamhound "1.5.5"]]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]
+                  [jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.10"]
                   [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-                  [lein-pprint "1.1.2"]
-                  [com.jakemccrary/lein-test-refresh "0.20.0"]]}
+                  [lein-pprint "1.1.2"]]}
  :repl {:plugins [[cider/cider-nrepl "0.15.0"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

d58e46153649da534173ce616df798b6b0e37ccd
Nate Smith <nwjsmith@gmail.com>
2017-10-11 11:40:45 -0400

    Clean up Leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,4 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :dependencies [[slamhound "1.5.5"]]
-        :plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-ancient "0.6.10"]
-                  [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]
-                  [lein-kibit "0.1.5" :exclusions [org.clojure/clojure]]
-                  [lein-pprint "1.1.2"]]}
- :repl {:plugins [[cider/cider-nrepl "0.15.0"]]
+        :dependencies [[slamhound "1.5.5"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.15.1"]]
         :dependencies [[org.clojure/tools.trace "0.7.9"]]}}

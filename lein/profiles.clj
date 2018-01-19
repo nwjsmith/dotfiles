a89edee740e10ff28e3316c0053a76bf67f6ddf5
Nate Smith <nwjsmith@gmail.com>
2018-01-19 14:36:44 -0500

    No need to bring my own CIDER

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,3 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :dependencies [[slamhound "1.5.5"]]}
- :repl {:plugins [[cider/cider-nrepl "0.15.1"]]
-        :dependencies [[org.clojure/tools.trace "0.7.9"]]}}
+ :repl {:dependencies [[org.clojure/tools.trace "0.7.9"]]}}

628f92b050c93e7c354c92e55720e8bf8a0554d7
Nate Smith <nwjsmith@gmail.com>
2018-08-19 15:00:32 -0400

    Leiningen plugin for LOC counts in Clojure

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :dependencies [[slamhound "1.5.5"]]}
+        :dependencies [[slamhound "1.5.5"]]
+        :plugins [[lein-count "1.0.9"]]}
  :repl {:dependencies [[org.clojure/tools.trace "0.7.9"]]}}

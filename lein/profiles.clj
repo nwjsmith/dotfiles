b9ff05d3394e9ee6b13c0665f1ed8a3e35111fc5
Nate Smith <nwjsmith@gmail.com>
2019-09-19 11:00:19 -0400

    Update Vim pack

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
         :dependencies [[slamhound "1.5.5"]]
         :plugins [[lein-count "1.0.9"]
                   [lein-ancient "0.6.15"]]}
+ :dev {:pedantic? :ranges}
  :repl {:dependencies [[org.clojure/tools.trace "0.7.10"]]}}

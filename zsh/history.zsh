e4dd2ccdbeb2b8ea5ac331d11acb3c63af161876
Nate Smith <nwjsmith@gmail.com>
2020-12-01 15:43:58 -0500

    ZSH history tweaks

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,8 +1,3 @@
-# Large history
-export HISTSIZE=100000
-export HISTFILESIZE=${HISTSIZE}
-setopt append_history
-setopt extended_history
-setopt share_history
-setopt hist_ignore_space
-setopt hist_ignore_all_dups
+setopt hist_ignore_all_dups inc_append_history
+export HISTSIZE=4096
+export SAVEHIST=4096

694bc70b4e1f8c0ef47104e4c869e63648dc2bba
Nate Smith <nwjsmith@gmail.com>
2021-05-20 16:44:53 -0400

    I'm DOOMin'

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,17 +1,24 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq user-full-name "Nate Smith"
-      user-mail-address "nate.smith@hey.com")
-
-(setq doom-theme 'doom-gruvbox-light
+(setq display-line-numbers-type t
       doom-font (font-spec :family "Rec Mono Linear" :size 12)
-      doom-variable-pitch-font (font-spec :family "Avenir Next"))
+      doom-theme 'doom-gruvbox-light
+      doom-variable-pitch-font (font-spec :family "Avenir Next")
+      org-directory "~/Documents/org"
+      org-roam-directory "~/Documents/org/roam"
+      user-full-name "Nate Smith"
+      user-mail-address "nate@theinternate.com")
 
-(setq display-line-numbers-type t)
-
-(setq-hook! 'typescript-mode-hook +format-with-lsp nil)
 (add-hook 'js2-mode-hook #'format-all-mode)
 (add-hook 'typescript-mode-hook #'format-all-mode)
 
+(setq-hook! 'typescript-mode-hook +format-with-lsp nil)
+
+(after! lsp-ui
+  (setq lsp-ui-sideline-show-code-actions nil))
+
 (after! vterm
   (setq vterm-term-environment-variable "eterm-color"))
+
+(after! cider
+  (setq cider-clojure-cli-aliases "dev"))
--- doom.d/init.el
+++ doom.d/init.el
@@ -21,7 +21,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +childframe) ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy +fuzzy       ; a search engine for love and life
@@ -92,13 +92,13 @@
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
+       gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
        (magit +forge)    ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
@@ -148,7 +148,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       ;;(org +roam)       ; organize your plain life in plain text
+       (org +noter +roam); organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional

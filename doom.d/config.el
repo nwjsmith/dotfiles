63eb3a586a1a018d7228e3e48ce7971b2d5c30bd
Nate Smith <nwjsmith@gmail.com>
2021-07-19 10:03:18 -0400

    Roam 2 is coming to DOOM
    Remove my custom configuration and just wait for it to be released

--- doom.d/config.el
+++ doom.d/config.el
@@ -15,41 +15,6 @@
 
 (setq-hook! 'typescript-mode-hook +format-with-lsp nil)
 
-(use-package! org-roam
-  :after org
-  :init
-  (map! :leader
-        :prefix ("n" . "notes")
-        (:prefix ("r" . "roam")
-         :desc "Toggle buffer" "b" #'org-roam-buffer-toggle
-         :desc "Capture" "c" #'org-roam-capture
-         :desc "Find node" "f" #'org-roam-node-find
-         :desc "Insert" "i" #'org-roam-node-insert
-         :desc "Random" "r" #'org-roam-node-random
-         :desc "Add tag" "t" #'org-roam-tag-add
-         :desc "Remove tag" "T" #'org-roam-tag-remove
-         :desc "Add alias" "a" #'org-roam-alias-add
-         :desc "Remove alias" "A" #'org-roam-alias-remove
-         (:prefix ("d" . "dailies")
-          :desc "Capture"   "c" #'org-roam-dailies-capture-date
-          :desc "Find date" "d" #'org-roam-dailies-find-date
-          :desc "Next"      "n" #'org-roam-dailies-find-next-note
-          :desc "Previous"  "p" #'org-roam-dailies-find-previous-note
-          :desc "Today"     "t" #'org-roam-dailies-find-today
-          :desc "Yesterday" "y" #'org-roam-dailies-find-yesterday)))
-  :config
-  (setq org-roam-directory (file-truename "~/Documents/org/roam")
-        org-roam-db-gc-threshold most-positive-fixnum)
-  (add-to-list 'display-buffer-alist
-               '(("\\*org-roam\\*"
-                  (display-buffer-in-direction)
-                  (direction . right)
-                  (window-width . 0.33)
-                  (window-height . fit-window-to-buffer)))))
-
-(use-package! org-roam-protocol
-  :after org-protocol)
-
 (after! lsp-ui
   (setq lsp-ui-sideline-show-code-actions nil))
 

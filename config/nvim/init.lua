3d058b2c11cb24c11f5b4e9b2cc2fd0768f16245
Nate Smith <nsmith@wealthsimple.com>
2022-03-07 13:59:48 -0500

    Fix backup option setting
    They aren't variables, but options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,9 +2,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- You are using Git, right?
-vim.g.backup = false
-vim.g.writebackup = false
-vim.g.swapfile = false
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
 
 vim.o.mouse = "a"
 

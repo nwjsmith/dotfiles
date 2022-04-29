4cab90c59775b1c378193881076d37fb02729785
Nate Smith <nate@theinternate.com>
2022-04-29 10:43:04 -0400

    Revert "Extract Neovim configuration into its own module"
    This reverts commit 3fac4b68ea1fb6bc9a24de5c46005aaa1a1a01f2.

--- home.nix
+++ home.nix
@@ -1,6 +1,12 @@
 { pkgs, config, ... }:
 
-{
+let
+  configuredVimPlugin = pkg: {
+    plugin = pkg;
+    config = builtins.readFile ./config/nvim/${pkg.pname}.lua;
+    type = "lua";
+  };
+in {
   home.packages = with pkgs; [
     asciinema
     awscli2
@@ -55,10 +61,46 @@
   programs.direnv.enable = true;
 
   xdg.configFile."nvim/nvim.lua".source = ./config/nvim/nvim.lua;
-  programs.neovim = import ./neovim.nix {
-    inherit config;
-    inherit pkgs;
+  programs.neovim = {
     enable = true;
+    plugins = with pkgs.vimPlugins; [
+      (configuredVimPlugin
+        (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars)))
+      (configuredVimPlugin conjure)
+      copilot-vim
+      direnv-vim
+      (configuredVimPlugin fzf-vim)
+      (configuredVimPlugin gitsigns-nvim)
+      (configuredVimPlugin gruvbox-nvim)
+      (configuredVimPlugin lualine-nvim)
+      nvim-colorizer-lua
+      (configuredVimPlugin nvim-lspconfig)
+      vim-commentary
+      vim-dispatch
+      vim-elixir
+      vim-eunuch
+      vim-fugitive
+      vim-git
+      vim-jack-in
+      vim-repeat
+      vim-rhubarb
+      vim-sexp
+      vim-sexp-mappings-for-regular-people
+      vim-sleuth
+      vim-speeddating
+      vim-surround
+      vim-test
+      vim-unimpaired
+      vim-vinegar
+      zoxide-vim
+    ];
+    extraConfig = ''
+      execute "luafile ${config.xdg.configHome}/nvim/nvim.lua"
+    '';
+    extraPackages = with pkgs; [
+      rubyPackages.solargraph
+      nodePackages.typescript-language-server
+    ];
   };
 
   programs.kitty = {

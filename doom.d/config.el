22147821e828208c4ad049e4da3f24445e41b616
Nate Smith <nate@theinternate.com>
2022-06-12 15:31:59 -0400

    Separate `setq`s for configuration

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,7 +1,9 @@
-(setq doom-theme 'doom-gruvbox-light
-      doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13)
-      doom-theme 'doom-gruvbox-light
-      doom-variable-pitch-font (font-spec :family "Inter")
-      typescript-indent-level 2)
+(setq doom-theme 'doom-gruvbox-light)
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 13))
+(setq doom-variable-pitch-font (font-spec :family "Inter"))
 
+;; TypeScript's default indent level is 4
+(setq typescript-indent-level 2)
+
+;; Only complete when asked
 (setq company-idle-delay nil)

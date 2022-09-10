3499096d68f42de17ac7b1bd7733134d13f36e4f
Nate Smith <nate@theinternate.com>
2022-09-10 10:13:07 -0400

    Extract ZSH module
    Putting my other shell stuff in here for now

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, ... }:
 
 {
-  imports = [ ./emacs.nix ./git.nix ./neovim.nix ];
+  imports = [ ./emacs.nix ./git.nix ./neovim.nix ./zsh.nix ];
 
   home.packages = with pkgs; [
     asciinema
@@ -21,38 +21,6 @@
     sqlite
     yt-dlp
   ];
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    MANPAGER = "nvim +Man!";
-    PAGER = "less -FR";
-  };
-
-  home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
-
-  programs.exa = {
-    enable = true;
-    enableAliases = true;
-  };
-
-  programs.fzf = {
-    enable = true;
-    defaultCommand = "${pkgs.fd}/bin/fd --type f";
-    defaultOptions = [
-      "--color=bg+:#ebdbb2,bg:#fbf1c7,spinner:#b57614,hl:#b57614"
-      "--color=fg:#3c3836,header:#bdae93,info:#076678,pointer:#076678"
-      "--color=marker:#af3a03,fg+:#3c3836,prompt:#7c6f64,hl+:#b57614"
-    ];
-    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
-    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
-  };
-
-  programs.bat = {
-    enable = true;
-    config = { theme = "gruvbox-light"; };
-  };
-
-  programs.direnv.enable = true;
 
   programs.kitty = {
     enable = true;
@@ -80,19 +48,6 @@
     theme = "Gruvbox Light";
   };
 
-  programs.zsh = {
-    enable = true;
-    enableAutosuggestions = false;
-    enableCompletion = true;
-    enableSyntaxHighlighting = true;
-    defaultKeymap = "viins";
-    initExtra = builtins.readFile ./zshrc;
-  };
-
-  programs.bash.enable = true;
-
-  programs.zoxide.enable = true;
-
   programs.gpg.enable = true;
   xdg.configFile."gnupg/gpg-agent.conf".text = ''
     enable-ssh-support
@@ -104,29 +59,6 @@
   xdg.configFile."shellcheckrc".source = ./shellcheckrc;
   xdg.configFile."karabiner/assets/complex_modifications/escape.json".source =
     ./config/karabiner/assets/complex_modifications/escape.json;
-
-  home.file.".local/bin/gem-constraint" = {
-    source = ./local/bin/gem-constraint;
-    executable = true;
-  };
-  home.file.".local/bin/gem-latest" = {
-    source = ./local/bin/gem-latest;
-    executable = true;
-  };
-
-  home.file.".local/bin/git-churn" = {
-    source = ./local/bin/git-churn;
-    executable = true;
-  };
-  home.file.".local/bin/git-original-branch" = {
-    source = ./local/bin/git-original-branch;
-    executable = true;
-  };
-
-  home.file.".local/bin/ordinalize" = {
-    source = ./local/bin/ordinalize;
-    executable = true;
-  };
 
   home.file.".lsp/config.edn".source = ./lsp/config.edn;
   home.file.".clojure/deps.edn".source = ./clojure/deps.edn;
---
+++ zsh.nix
@@ -0,0 +1,67 @@
+{ pkgs, config, ... }:
+
+{
+  programs.direnv.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = false;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    defaultKeymap = "viins";
+    initExtra = builtins.readFile ./zshrc;
+  };
+  programs.zoxide.enable = true;
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    MANPAGER = "nvim +Man!";
+    PAGER = "less -FR";
+  };
+
+  home.sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
+
+  programs.exa = {
+    enable = true;
+    enableAliases = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    defaultCommand = "${pkgs.fd}/bin/fd --type f";
+    defaultOptions = [
+      "--color=bg+:#ebdbb2,bg:#fbf1c7,spinner:#b57614,hl:#b57614"
+      "--color=fg:#3c3836,header:#bdae93,info:#076678,pointer:#076678"
+      "--color=marker:#af3a03,fg+:#3c3836,prompt:#7c6f64,hl+:#b57614"
+    ];
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
+  };
+
+  programs.bat = {
+    enable = true;
+    config = { theme = "gruvbox-light"; };
+  };
+
+  home.file.".local/bin/gem-constraint" = {
+    source = ./local/bin/gem-constraint;
+    executable = true;
+  };
+  home.file.".local/bin/gem-latest" = {
+    source = ./local/bin/gem-latest;
+    executable = true;
+  };
+
+  home.file.".local/bin/git-churn" = {
+    source = ./local/bin/git-churn;
+    executable = true;
+  };
+  home.file.".local/bin/git-original-branch" = {
+    source = ./local/bin/git-original-branch;
+    executable = true;
+  };
+
+  home.file.".local/bin/ordinalize" = {
+    source = ./local/bin/ordinalize;
+    executable = true;
+  };
+
+}

1834b1ca2365abd7685f64122795a71a8e600dde
Nate Smith <nate@theinternate.com>
2022-09-19 15:06:32 -0400

    Learn Rust
    Don't want to be left behind 😅

--- doom.d/init.el
+++ doom.d/init.el
@@ -165,7 +165,7 @@
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        (ruby +rails +tree-sitter) ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp +tree-sitter) ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        (sh +tree-sitter) ; she sells {ba,z,fi}sh shells on the C xor

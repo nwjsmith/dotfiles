aca2921000b5d0deb0c70f0b4eac99312e00dbf0
Nate Smith <nate@theinternate.com>
2023-08-24 10:24:10 -0400

    Update flake.lock and flake.nix
    This commit updates the flake.lock and flake.nix files. The changes
    include updated revisions and narHash values for various packages, such
    as flake-utils, home-manager, nix-darwin, and nixpkgs. These updates
    ensure that the dependencies used in the project are up-to-date,
    improving stability and security.

--- flake.nix
+++ flake.nix
@@ -14,41 +14,49 @@
     };
   };
 
-  outputs = { self, nix-darwin, home-manager, nixpkgs, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-    in {
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = with pkgs; [ alejandra nil ];
-      };
-      darwinConfigurations.workbook = nix-darwin.lib.darwinSystem {
-        inherit pkgs system;
-
-        modules = [
-          home-manager.darwinModules.home-manager
-          ./darwin.nix
-          ({ ... }: {
-            users.users.nsmith.home = "/Users/nsmith";
-            home-manager = {
-              useGlobalPkgs = true;
-              users.nsmith = import ./home.nix;
-            };
-          })
-        ];
-      };
-      homeConfigurations.nsmith = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        modules = [
-          ./home.nix
-          {
-            home = {
-              username = "nsmith";
-              homeDirectory = "/Users/nsmith";
-            };
-          }
-        ];
-      };
+  outputs = {
+    self,
+    nix-darwin,
+    home-manager,
+    nixpkgs,
+    ...
+  }: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {allowUnfree = true;};
     };
+  in {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [alejandra nil];
+    };
+    darwinConfigurations.workbook = nix-darwin.lib.darwinSystem {
+      inherit pkgs system;
+
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./darwin.nix
+        ({...}: {
+          users.users.nsmith.home = "/Users/nsmith";
+          home-manager = {
+            useGlobalPkgs = true;
+            users.nsmith = import ./home.nix;
+          };
+        })
+      ];
+    };
+    homeConfigurations.nsmith = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [
+        ./home.nix
+        {
+          home = {
+            username = "nsmith";
+            homeDirectory = "/Users/nsmith";
+          };
+        }
+      ];
+    };
+  };
 }
